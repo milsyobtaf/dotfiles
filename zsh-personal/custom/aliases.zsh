ecfddbffd7a8ee7ffffb7f2cb66c41ff55e29b10
Dustin Younse <milsyobtaf@gmail.com>
2015-11-09 09:09:26 -0500

    Moved functions into separate partial

--- zsh-personal/custom/aliases.zsh
+++ zsh-personal/custom/aliases.zsh
@@ -1,41 +1,3 @@
-# cd to the path of the front Finder window
-cdf() {
-  target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
-  if [ "$target" != "" ]; then
-    cd "$target"; pwd
-  else
-    echo 'No Finder window found' >&2
-  fi
-}
-
-# open a specified man page in Preview
-man-preview() {
-  man -t "$@" | open -f -a Preview
-}
-
-# move a specified file to the OSX Trash
-trash() {
-  local trash_dir="${HOME}/.Trash"
-  local temp_ifs=$IFS
-  IFS=$'\n'
-  for item in "$@"; do
-    if [[ -e "$item" ]]; then
-      item_name="$(basename $item)"
-      if [[ -e "${trash_dir}/${item_name}" ]]; then
-        mv -f "$item" "${trash_dir}/${item_name} $(date "+%H-%M-%S")"
-      else
-        mv -f "$item" "${trash_dir}/"
-      fi
-    fi
-  done
-  IFS=$temp_ifs
-}
-
-# shortcut to get figlet text for IRC, etc
-shout() {
-  figlet "$@" | pbcopy
-}
-
 # list out apps installed by homebrew
 alias brews='brew list'
 
@@ -77,4 +39,3 @@ alias subl='atom'
 
 # old drush instead of new drush
 alias drush7='/usr/local/Cellar/drush/7.0.0/bin/drush'
-
