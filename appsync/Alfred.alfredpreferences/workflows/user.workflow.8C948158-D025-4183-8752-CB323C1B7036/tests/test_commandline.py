2286df2e67af4823e5b44d90c0c566ffee942c46
Dustin Younse <milsyobtaf@gmail.com>
2023-03-28 10:25:31 -0500

    Remove unnecessary Alfred prefs

--- appsync/Alfred.alfredpreferences/workflows/user.workflow.8C948158-D025-4183-8752-CB323C1B7036/tests/test_commandline.py
+++
@@ -1,6 +0,0 @@
-from converter import main
-
-
-def test_convert():
-    main.scriptfilter('5 * sin(pi/2) + 2 mm in m')
-
--- appsync/Alfred.alfredpreferences/workflows/user.workflow.AFBF530C-DE30-4A34-9276-CEB0713AEF33/rebuild.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'transmit'
-
-transmit = Transmit.new
-transmit.rebuild_cache
-puts 'Favorites cache rebuilt'
